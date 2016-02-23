580b0927cf7d1c6ee7c532df1d467422f9bc82c0
Gustavo Gutierrez <gustavogut1993@gmail.com>
2016-02-23 13:14:46 -0430

    Inicializado con Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interfacesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the interfacesApp
+ */
+angular.module('interfacesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name interfacesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the interfacesApp
+ */
+angular.module('interfacesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
