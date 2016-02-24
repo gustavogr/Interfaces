1f69828283ca00d0e84b4ff7f2c4ac3e6b2be86d
Gustavo Gutierrez <gustavogut1993@gmail.com>
2016-02-24 09:00:12 -0430

    Vista de Login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,10 +26,15 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl',
+        controllerAs: 'profile'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
       })
       .otherwise({
         redirectTo: '/'
